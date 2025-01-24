c47410636e5b96ffc28a7598d57ebadf8f367c5b
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-25 01:26:45 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from api.cruds import item as item_cruds
+
 
 app = FastAPI()
 
-@app.get("/")
-async def example():
-    return{"message":"Hello world!"}
+@app.get("/items")
+async def find_all():
+    return item_cruds.find_all()
